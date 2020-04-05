d24cb0ffecfc82d791cbd3137259584814972e8d
glocore <mailtoashwinnayar@gmail.com>
2020-04-05 21:44:37 +0530

    Updates

---
+++ precache-manifest.91148167ac15d1b612f476c8055af4c6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "047e1f6d7415b27ae267bad157480471",
+    "url": "/35mm/index.html"
+  },
+  {
+    "revision": "c969b79558cc1f573c48",
+    "url": "/35mm/static/css/2.3fff7fcf.chunk.css"
+  },
+  {
+    "revision": "21256fca803f6eaeaeb5",
+    "url": "/35mm/static/css/main.f207a4c4.chunk.css"
+  },
+  {
+    "revision": "c969b79558cc1f573c48",
+    "url": "/35mm/static/js/2.ad9068f6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/35mm/static/js/2.ad9068f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21256fca803f6eaeaeb5",
+    "url": "/35mm/static/js/main.bdea316b.chunk.js"
+  },
+  {
+    "revision": "6340cb580f20a650c153",
+    "url": "/35mm/static/js/runtime-main.2daf1f1b.js"
+  },
+  {
+    "revision": "22545845e23c778f557dd876e86bc7c8",
+    "url": "/35mm/static/media/add_icon.22545845.svg"
+  },
+  {
+    "revision": "175db760bd2199722b541f71d357b09f",
+    "url": "/35mm/static/media/remove_icon.175db760.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/35mm/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/35mm/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/35mm/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/35mm/static/media/slick.f97e3bbf.svg"
+  }
+]);
